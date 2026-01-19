c50bee4f9af496d41852a1c6c39192e8e07f9993
CHUIZ OSORIO JUAN DAVID <202214036@uns.edu.pe>
2026-01-19 08:45:42 -0500

    Inicializacion del Proyecto

---
+++ src/main/java/com/naal/bankmind/BankMindApplication.java
@@ -0,0 +1,13 @@
+package com.naal.bankmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naal/bankmind/BankMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naal.bankmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

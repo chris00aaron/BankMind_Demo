0520e0d67584992e398d252fd534c31185404bff
Angelo <angelomejia970@gmail.com>
2026-02-05 10:28:15 -0500

    Factorización y eliminación de codigo repetido para Fraude

--- src/main/java/com/naal/bankmind/repository/Fraud/TransactionRepository.java
+++ src/main/java/com/naal/bankmind/repository/Fraud/TransactionRepository.java
@@ -1,15 +1,11 @@
 package com.naal.bankmind.repository.Fraud;
 
 import com.naal.bankmind.entity.OperationalTransactions;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.List;
 import java.util.Optional;
 
